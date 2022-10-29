2b535a96c6ef43202243d7a628289d77cd7ab8b7
PANKAJ KUMAR BARMAN <59820934+573-pankaj@users.noreply.github.com>
2022-10-29 09:02:18 +0530

    Add files via upload

---
+++ Basic C++/1.helloworld.cpp
@@ -0,0 +1,17 @@
+#include<iostream> // preprocessor directive used to include files
+         // heder files for taking inputs and printing outputs
+using namespace std;
+int main(){ // the execution of the code start from main function
+
+cout<<"hello world \n";// ; marke end of the statement
+int a,b;
+// cout << " Enter a Numer1  = "<< endl;
+// cin>>a;
+// cout<< " Enter a number2  = " << endl;
+// cin>>b;
+cin>>a>>b;
+cout<<"sum : "<<a+b;
+
+return 0;// exits status of the function
+
+}    
